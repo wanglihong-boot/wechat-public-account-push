ac79d89bee39e960aa449113d88e839f44d7f8d1
Spring-boot <71641300+wanglihong-boot@users.noreply.github.com>
2024-05-03 18:29:15 +0800

    Update index.cjs
    1

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ovmV36kWxNX7FeXhpe0d-6g-8PJw',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '	GTH3rKTarMZT5QVyn1eg4UbmWdGhAmKMYjgFPC4gFN4',
+      useTemplateId: 'GTH3rKTarMZT5QVyn1eg4UbmWdGhAmKMYjgFPC4gFN4',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '	GTH3rKTarMZT5QVyn1eg4UbmWdGhAmKMYjgFPC4gFN4',
+  CALLBACK_TEMPLATE_ID: 'GTH3rKTarMZT5QVyn1eg4UbmWdGhAmKMYjgFPC4gFN4',
 
   CALLBACK_USERS: [
     {
